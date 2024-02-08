c02daa6c09e7df0c8812723fec66d5adbeafe8c8
Michael <michaeljohn@apollotech.co>
2024-02-08 17:50:21 +0800

    m02

---
+++ cypress/e2e/SBS/Masterfile/User/m02-user_tab.cy.js
@@ -0,0 +1,98 @@
+/// <reference types="cypress" />
+
+//Common Functions
+function navigateToModule(module){
+    cy.get('ul').contains(module).click();
+}
+
+function navigateToSubModule(subModule){
+    cy.get('li').contains(subModule).click();
+}
+
+function navigateToTableModule(tableName) {
+    cy.get('h3').contains('Show User');
+    cy.get('li').find('a').contains(tableName).click();
+}
+
+function searchWithOneField(fieldId,value){
+    const field = `[id^=${fieldId}]`;
+    cy.get(field).type(value);
+    cy.get('.btn').contains('Search').click();
+}
+
+function searchSuccess(data, check = false) {
+    const keys = Object.keys(data); 
+    keys.forEach(key => {
+        if(data[key] != "") { 
+            searchWithOneField(key, data[key]);
+            if(check) {
+                cy.get('td').find('a').contains(data[key]);
+            } else {
+                cy.get('.message').should('contain', 'Result not found.');
+            }
+            cy.get('.btn').contains('Clear').click();
+        }
+    });
+}
+
+function navigateThenBack(data) {
+    const keys = Object.keys(data); 
+    keys.forEach(key => {
+        if(data[key] != "") {
+            cy.get('td').find('a').contains(data[key]).click(); 
+            cy.get('.btn').contains('<< Back to').click(); 
+        }
+    });
+}
+
+function login() {
+    beforeEach(() => {
+        cy.visit('http://192.168.64.3:8080/RetailPlusStoreBackend/login/auth')
+        cy.contains('Username');
+        cy.contains('Password');
+        cy.contains('Login');
+        cy.fixture('sbs_credentials/sbs_credentials.json').then((login_data) =>{
+            cy.get('[id^=username]').type(login_data.username);
+            cy.get('[id^=password]').type(login_data.password);
+        })
+        cy.get('[id^=submit]').click();
+
+        cy.contains('Masterfile');
+        cy.contains('Matrix');
+        cy.contains('Inventory');
+        cy.contains('Sales');
+        cy.contains('Report');
+        cy.contains('Misc');
+        cy.contains('Sign out');
+    })
+}
+
+context('Masterfile -> User', () => {
+        login();
+
+        it('Search User', () => {
+        navigateToModule('Masterfile');
+        navigateToSubModule('User'); 
+
+        cy.fixture('masterfile/user/search_user_tab_data').then((data) => {
+            searchSuccess(data[0]);
+            searchSuccess(data[1], true);
+
+            cy.get('td').find('a').contains(data[2].externalId).click();
+
+            // Go to Contact Info
+            navigateToTableModule('Contact Info');
+            navigateThenBack(data[2].data[0]);
+
+            // Go to Facility User Role
+            navigateToTableModule('Facility User Role');
+            navigateThenBack(data[2].data[1]);
+
+            // Go to Facility User Role
+            navigateToTableModule('Party Info');
+            navigateThenBack(data[2].data[2]);
+
+            cy.get('.btn').contains('<< Back to').click(); 
+        });
+    });
+})
