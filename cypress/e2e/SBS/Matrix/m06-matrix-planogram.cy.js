a250666a76370b717a20ee05694554b1ff519ee3
Michael <michaeljohn@apollotech.co>
2024-02-12 15:31:28 +0800

    m06 done

---
+++ cypress/e2e/SBS/Matrix/m06-matrix-planogram.cy.js
@@ -0,0 +1,128 @@
+/// <reference types="cypress" />
+
+//Common Functions
+function navigateToModule(module){
+    cy.get('ul').contains(module).click();
+}
+
+function navigateToSubModule(subModule){
+    cy.get('li').contains(subModule).click();
+}
+
+function navigateToTableModule(tableName) {
+    cy.get('h3').contains('Show Planogram');
+    cy.get('.nav-tabs').find('a').contains(tableName).click();
+}
+
+function searchWithOneField(fieldId,value){
+    const field = `[id^=${fieldId}]`;
+    cy.get(field).type(value);
+    cy.get('.btn').contains('Search').click();
+}
+
+function searchWithCategory(fieldId,value){
+    const field = `[id^=${fieldId}]`;
+    cy.get(field).select(value);
+    cy.get('.btn').contains('Search').click();
+}
+
+function searchSuccess(data, check = false, category = false) {
+    const keys = Object.keys(data); 
+    keys.forEach(key => {
+        if(data[key] != "") { 
+            if(category) {
+                searchWithCategory(key, data[key]);
+            } else {
+                searchWithOneField(key, data[key]);
+            }
+            if(check) {
+                cy.get('table').should('have.descendants', 'td');
+            } else {
+                cy.get('.message').should('contain', 'Result not found.');
+            }
+            cy.get('.btn').contains('Clear').click();
+        }
+    });
+}
+
+function navigateThenBack(data) {
+    const keys = Object.keys(data); 
+    keys.forEach(key => {
+        if(data[key] != "") {
+            cy.get('td').find('a').contains(data[key]).click(); 
+            cy.get('.btn').contains('<< Back to').click(); 
+        }
+    });
+}
+
+function login() {
+    beforeEach(() => {
+        cy.visit('http://192.168.64.3:8080/RetailPlusStoreBackend/login/auth')
+        cy.contains('Username');
+        cy.contains('Password');
+        cy.contains('Login');
+        cy.fixture('sbs_credentials/sbs_credentials.json').then((login_data) =>{
+            cy.get('[id^=username]').type(login_data.username);
+            cy.get('[id^=password]').type(login_data.password);
+        })
+        cy.get('[id^=submit]').click();
+
+        cy.contains('Masterfile');
+        cy.contains('Matrix');
+        cy.contains('Inventory');
+        cy.contains('Sales');
+        cy.contains('Report');
+        cy.contains('Misc');
+        cy.contains('Sign out');
+    })
+}
+
+context('Matrix -> Planogram', () => {
+    login();
+
+    it('TC01: S01 - S05', () => {
+        navigateToModule('Matrix');
+        navigateToSubModule('Planogram'); 
+
+        cy.fixture('matrix/planogram_data/search_planogram_tab_data').then((data) => {
+            searchSuccess(data[0]); // incorrect
+            searchSuccess(data[1], true); // correct
+            searchSuccess(data[2], true, true); // correct, category
+
+            searchSuccess(data[4].data[0], false, true);
+            searchSuccess(data[4].data[1], false, true);
+            searchSuccess(data[4].data[2], false, true);
+            searchSuccess(data[4].data[3], true, true);
+            searchSuccess(data[4].data[4], false, true);
+            searchSuccess(data[4].data[5], false, true);
+
+            searchWithOneField('f_documentId', data[1].f_documentId);
+            cy.get('td').find('a').contains(data[1].f_documentId).click();
+
+            cy.get('.btn').contains('Print').click(); 
+        
+            cy.get('td').find('a').contains(data[3].f_locationId).click();
+            cy.get('.errors').should('contain', "Sorry, you're not authorized to view this page.");
+        });
+        
+    });
+
+    it('TC01: S06 - S08', () => {
+        navigateToModule('Matrix');
+        navigateToSubModule('Planogram'); 
+
+        cy.fixture('matrix/planogram_data/search_planogram_tab_data').then((data) => {
+            navigateToModule('Matrix');
+            navigateToSubModule('Planogram'); 
+            searchWithOneField('f_documentId', data[1].f_documentId);
+            cy.get('td').find('a').contains(data[1].f_documentId).click();
+            cy.get('.pagination > :nth-child(2)').click()
+                
+            navigateToTableModule('Product');
+            cy.get('.pagination > :nth-child(2)').click()
+
+            cy.get('.btn').contains('<< Back to').click();
+            cy.get('.pagination > :nth-child(2)').click() 
+        });
+    });
+})
